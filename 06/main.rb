788984a0002056d403c88925fa98e5bdb3879de9
Josh Spicer <josh@joshspicer.com>
2022-12-06 16:31:24 +0000

    day 6 part 1 and 2

--- 06/main.rb
+++ 06/main.rb
@@ -5,12 +5,36 @@ FILENAME='input.txt' # example.txt
 
 input_file = File.new(FILENAME, "r") # example.txt
 
-def parse(line)
-    buffer = Array.new(4)
-    
+def parse(line, count)
+    queue = []
+    line.split("").each do |char|
+      if queue.length < count
+        queue.unshift(char)
+        next
+      end
 
+      if queue.length == count
+        if queue & queue == queue
+          pattern = queue.reverse!.join("")
+
+          puts pattern
+          idx = line.index(pattern) + count
+          puts idx
+          return
+        end
+      end
+
+      queue.pop
+      queue.unshift(char)
+    end
 end
 
 for line in input_file
-  puts parse(line)
+  puts "Part 1"
+  puts "------"
+  puts parse(line, 4)
+
+  puts "Part 2"
+  puts "------"
+  puts parse(line, 14)
 end
