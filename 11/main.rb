7f4e2eb983cc4a83684446ea7f199dd46edce6b9
Josh Spicer <josh@joshspicer.com>
2022-12-13 03:23:18 +0000

    day 11 part 1

--- 11/main.rb
+++ 11/main.rb
@@ -12,19 +12,45 @@ if arg == 'example'
     # Example
     FILENAME='example.txt' 
     NUM_MONKEYS=4
+    ROUNDS = 20
+    DEBUG = true
 elsif arg == 'input'
     # Input
     FILENAME='input.txt' 
-    NUM_MONKEYS=7
+    NUM_MONKEYS=8
+    ROUNDS = 20
+    DEBUG = false
 else
     puts "Unknown argument: '#{arg}'"
     exit
 end
 
 class Operation
+    attr_reader :operator, :operand
+
     def initialize(operator, operand)
         @operator = operator
         @operand = operand # Can be a number, -or- the 'old' value
+    end
+
+    def apply(old_value)
+        if @operand == 'old'
+            operand = old_value
+        else
+            operand = @operand.to_i
+        end
+
+        if @operator == '*'
+            result = old_value * operand
+            puts "    Worry level is multiplied by #{operand} to #{result}" if DEBUG
+            return result
+        elsif @operator == '+'
+            result = old_value + operand
+            puts "    Worry level is increases by #{operand} to #{result}" if DEBUG
+            return result
+        elsif
+            raise "Unknown operator: #{@operator}"
+        end
     end
 
     def pretty_print
@@ -33,10 +59,23 @@ class Operation
 end
 
 class TestFunction
+    attr_reader :divisible_by, :throw_to_x_if_true, :throw_to_x_if_false
+
     def initialize(divisible_by, throw_to_x_if_true, throw_to_x_if_false)
         @divisible_by = divisible_by
         @throw_to_x_if_true = throw_to_x_if_true
         @throw_to_x_if_false = throw_to_x_if_false
+    end
+
+    # The number of the monkey to throw to (1-indexed)
+    def test(worry_level)
+        if worry_level % @divisible_by.to_i == 0
+            puts "    Current worry level is divisible by #{@divisible_by}." if DEBUG
+            return @throw_to_x_if_true.to_i
+        else
+            puts "    Current worry level is not divisible by #{@divisible_by}." if DEBUG
+            return @throw_to_x_if_false.to_i
+        end
     end
 
     def pretty_print
@@ -47,17 +86,22 @@ class TestFunction
 end
 
 class Monkey
+    attr_reader :operation, :test_func
+    attr_accessor :items, :inspect_count
+
     def initialize(starting_items, operation, test_func)
         @items = starting_items # Array
         @operation = operation
-        @test = test_func
+        @test_func = test_func
+
+        @inspect_count = 0
     end
 
     def pretty_print
         puts "Monkey:"
         puts "Starting items: #{@items.join(', ')}"
         @operation.pretty_print
-        @test.pretty_print
+        @test_func.pretty_print
     end
 end
 
@@ -96,16 +140,55 @@ File.open(FILENAME) do |file|
         test_function = TestFunction.new(divisible_by, throw_to_x_if_true, throw_to_x_if_false)
 
         # Create Monkey
-        monkey = Monkey.new(start_items, operation, test_function)
-        monkeys << monkey
+        monkeys << Monkey.new(start_items, operation, test_function)
 
         # Swallow new line
         file.gets
     end
 end
 
+# Part 01
+for round in 1..ROUNDS
+    for monkey in monkeys
+        num_items = monkey.items.length
 
-for monkey in monkeys
-    monkey.pretty_print
-    puts
+        # if DEBUG
+        #     puts "~~~~~~"
+        #     for i in 0..NUM_MONKEYS-1
+        #         puts "Monkey #{i + 1}: #{monkeys[i].items.join(', ')}"
+        #     end
+        #     puts "~~~~~~"
+        # end
+
+        puts "Monkey #{monkeys.index(monkey)}" if DEBUG
+
+        for i in 1..num_items
+            # Pop first item from monkey's list
+            item = monkey.items.shift
+            puts "  Monkey inspects an item with a worry level of #{item}" if DEBUG
+
+            monkey.inspect_count += 1
+
+            # Apply operation
+            worry_level = monkey.operation.apply(item)
+
+            # Divide by 3 and floor
+            worry_level = worry_level / 3
+            puts "    Monkey gets bored with item. Worry level is divided by 3 to #{worry_level}" if DEBUG
+
+            # Test worry level
+            throw_to_monkey_number = monkey.test_func.test(worry_level)
+            puts "    Item with worry level #{worry_level} is thrown to monkey #{throw_to_monkey_number}." if DEBUG
+
+            # Throw to monkey
+            monkeys[throw_to_monkey_number].items << worry_level
+        end
+    end
 end
+
+# Part 01
+# Get the two monkeys with the largest inspect_count
+inspect_counts = monkeys.map { |monkey| monkey.inspect_count }
+inspect_counts.sort!
+inspect_counts.reverse!
+puts "Part 01: #{inspect_counts[0] * inspect_counts[1]}"
